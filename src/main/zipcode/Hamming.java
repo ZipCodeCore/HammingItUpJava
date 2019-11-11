40c94c755f511787278c416730c8d1c9f7144d70
David Trombello <david@MacBook-Pro-8.localdomain>
2019-11-10 19:46:22 -0500

    cleaned up code

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -6,18 +6,15 @@ import java.util.logging.Logger;
 public class Hamming {
     private String s;
     private String s1;
-    //private static final Logger logger = new Logger(System.in, System.out);
 
-    public Hamming(String s, String s1) {
+    public Hamming(String s, String s1) throws IllegalArgumentException {
         this.s = s;
         this.s1 = s1;
         if (s.length() != s1.length()){
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length."); }
     }
 
-    public int getHammingDistance() throws IllegalArgumentException {
-
-
+    public int getHammingDistance()  {
 
         Integer hammingValue = 0;
         String [] sSplit = s.split("");
@@ -27,7 +24,6 @@ public class Hamming {
                 for (int i = 0; i < sSplit.length; i++){
                     if (!sSplit[i].equals(s1Split[i])){
                         hammingValue++;
-
                     }
                 }
             }
