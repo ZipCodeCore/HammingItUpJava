55e6c0d4247c730a9e8e211d88122e0d77742e7b
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-11-12 09:44:09 -0500

    Completed Lab

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,35 @@
 package zipcode;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.util.logging.Logger;
+
 public class Hamming {
-    public Hamming(String s, String s1) {
+    String s;
+    String s1;
+
+
+    public Hamming(String s, String s1)throws IllegalArgumentException {
+        this.s = s;
+        this.s1 = s1;
+        if(s.length()!=s1.length()){
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
-    public int getHammingDistance() {
-        return -1;
-    }
+    public int getHammingDistance(){
+            int hammy = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i)!=s1.charAt(i)){
+                    hammy++;
+                }
+
+            }
+            return hammy;
+        }
+
+
+
+
 }
