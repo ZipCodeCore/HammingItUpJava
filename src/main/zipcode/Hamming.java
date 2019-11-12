38184251f248fd3006e1e8de2d6bf8cc7bcafa87
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-12 18:21:04 -0500

    Add logic for identical strands

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -2,14 +2,29 @@ package zipcode;
 
 public class Hamming {
 
+    private String leftStrand;
+    private String rightStrand;
+
+    //-----------  constructor ----------------------
     public Hamming(String s, String s1) {
+        leftStrand = s;
+        rightStrand = s1;
 
         if (s.length() < s1.length() || s.length() > s1.length()){
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         }
+
+        getHammingDistance();
     }
 
+//---------------------------------------------------
+
     public int getHammingDistance() {
+
+        if (leftStrand.equals(rightStrand)) return 0;
         return -1;
     }
+
+
+
 }
