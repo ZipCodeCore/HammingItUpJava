0aad24b8f3d616528c81e506438a878593743eb4
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-4.local>
2019-11-13 15:34:58 -0500

    Complete

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,29 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+
+    private String s1;
+    private String s;
+
+
+    public Hamming(String s, String s1) throws IllegalArgumentException {
+        this.s = s;
+        this.s1 = s1;
+
+        if(s.length() != s1.length()) {
+            throw new  IllegalArgumentException ("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        Integer counter = 0;
+        if (s1.length() == s.length()) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s1.charAt(i) != s.charAt(i)) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 }
