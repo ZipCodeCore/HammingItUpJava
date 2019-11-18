851fcd9a8a0b54f4869aefd33f84404f8d7b4626
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-17 20:31:41 -0500

    complete

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,27 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+
+    String s;
+    String s1;
+
+    public Hamming(String s, String s1) throws IllegalArgumentException {
+        this.s = s;
+        this.s1 = s1;
+
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int hDistance = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s1.charAt(i)) {
+                hDistance++;
+            }
+        }
+        return hDistance;
     }
 }
